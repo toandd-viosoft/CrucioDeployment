0f8feb406ef001d5796d510ca6f2e62159b15104
toandd-viosoft <toandd@viosoft.com>
2017-07-21 14:27:04 +0700

    Validium supported adjusting parameters
    Don't need to hardcode

--- setup/11_install_dats.sh
+++ setup/11_install_dats.sh
@@ -33,16 +33,18 @@ if [ $DATS_VER = 7312cd45ad0eff9fda3360e01d8c9f415ed42fe5 ]
 then
 	cd /opt/crucio
 	patch -d DATS/tests/ -p1 < /opt/patch-files/common-patchs/patch/dats/adjust-mempool-for-more-rxtx-desc.patch
-
+        
+	# From 21-07-2017, validium supported adjusting parameters
+	# don't need to hardcode
 	# Gettign new patchs for DATS
-	cd DATS/
-	mv tests/prox-configs/ tests/prox-configs.old/
-	rm -rf dats-patchs/
-	git clone https://github.com/toandd-viosoft/dats-patchs.git
-	cd dats-patchs
-	git checkout 06a31252adac6a3e4d47e9743de6f941dc97376c
-	cd ..
-	cp -r dats-patchs/baremetal-patchs/prox-configs/ tests/
-	chmod +x dats.py
+	# cd DATS/
+	# mv tests/prox-configs/ tests/prox-configs.old/
+	# rm -rf dats-patchs/
+	# git clone https://github.com/toandd-viosoft/dats-patchs.git
+	# cd dats-patchs
+	# git checkout 06a31252adac6a3e4d47e9743de6f941dc97376c
+	# cd ..
+	# cp -r dats-patchs/baremetal-patchs/prox-configs/ tests/
+	# chmod +x dats.py
 fi
 
